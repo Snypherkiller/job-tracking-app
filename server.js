9c09a0d182bd3ad8754bbcbe4a3e4e4d8122c2cf
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-12-08 00:04:45 +0530

    nn

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import notFoundMiddleware from './middlewares/not-found.js'
 const app=express()
+
+
+
+app.get('/', (req,res)  => {
+    res.send('Welcome')
+})
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
