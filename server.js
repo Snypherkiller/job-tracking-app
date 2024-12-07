2ed83e1219d0471f112d39bbadc6845635a249dc
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-12-07 12:55:44 +0530

    h

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Server Running')
+import express from 'express'
+const app=express()
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+
+    console.log (`The app is running on port ${port}`)
+})
