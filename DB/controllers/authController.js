9c48f702bd01aa182c6ed096ed9b2d6200fc814c
Snypherkiller <159802996+Snypherkiller@users.noreply.github.com>
2024-12-16 19:20:34 +0530

    new

--- DB/controllers/authController.js
+++ DB/controllers/authController.js
@@ -4,16 +4,8 @@ import mongoose from "mongoose"
 
 const register =async(req,res,next)=>
 {
-    try {
-
-        const user = await User.create(req.body)
-        res.status(201).json({user})
-        
-    } catch (error) {
-        console.log(error)
-        next(error)
-        
-    }
+    const user = await User.create(req.body)
+    res.status(201).json({user})
 }
 const login =async(req,res)=>
 {
